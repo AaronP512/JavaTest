65f7e2df771c0e2ead181aeaf0dacb8530194202
AaronP512 <acp.aaron512@gmail.com>
2017-07-16 04:25:31 +0530

    gui programming start

---
+++ firstgui.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+public class firstgui extends JFrame {
+	public static void main(String[] args) {
+		firstgui gui = new firstgui();
+		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gui.setSize(400, 400);
+		gui.setVisible(true);
+		gui.setTitle("First GUI");
+	}
+	
+
+}
+ 
